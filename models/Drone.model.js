dfe339076d60dc787eacc92556ff3abdaaa399cf
nagyrobert92 <nagyrobert1992@yahoo.com>
2021-04-17 18:29:55 +0200

    iteration 1 done

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,13 @@
 // Iteration #1
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema;
+
+const droneMod = new Schema ({
+    name: String,
+    propellers: Number,
+    maxSpeed: Number
+})
+
+const Drone = mongoose.model("Drone", droneMod)
+
+module.exports = Drone;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,20 @@
 // Iteration #1
+const mongoose = require("mongoose");
+const DroneMod = require('../models/Drone.model')
+
+require('../db/index')
+
+const drones = [
+    { name: 'Creeper XL 500', propellers: 3, maxSpeed: 12 },
+    { name: 'Racer 57', propellers: 4, maxSpeed: 20 },
+    { name: 'Courier 3000i', propellers: 6, maxSpeed: 18 }
+  ];
+
+  DroneMod.create(drones)
+  .then((listOfDrones) => {
+      console.log(`${listOfDrones.length} were created`)
+      mongoose.disconnect();
+  })
+  .catch((err) => {
+    console.log(err)
+  })
